5baf6ce2b3d62e7c202f7a2fa0b6f0ee9ae2458e
Tharuka Lakshan <tharukawapnet@gmail.com>
2020-12-31 18:43:49 +0530

    latest

--- src/app/views/dashboard/inquiry/inquiry.component.ts
+++ src/app/views/dashboard/inquiry/inquiry.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { config } from 'config';
 
 @Component({
   selector: 'app-inquiry',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InquiryComponent implements OnInit {
 
-  constructor() { }
+  selectedIndex = 1;
+  editMode = false;
+  editingObject = {};
+  class = new FormGroup({
+    inquiry: new FormControl('', Validators.required)
+  })
+
+  dataColumns = [];
+
+  dataSource:any = [];
+
+  constructor(private http:HttpClient, private snackBar:MatSnackBar) {}
+
+  updateSelect(data){
+    this.selectedIndex = 0;
+    this.editMode = true;
+    this.editingObject = data;
+  }
+
+  getAllClasses(){
+    this.http.get(`${config.apiUrl}/inquires`).subscribe((res:any)=>{
+      this.dataSource = res.data;
+    });
+  }
+
+  cancelEditMode(){
+    this.editMode = false;
+  }
+
+  newClass(){
+    let data = {
+      inquiry: this.class.get("inquiry").value
+    };
+
+    this.http.post(`${config.apiUrl}/inquires`, data).subscribe(res=>{
+      this.getAllClasses();
+      this.snackBar.open("Created", "Ok", {duration:2000});
+      this.class.reset();
+    });
+  }
 
   ngOnInit(): void {
+    this.dataColumns = ["id", "inquiry"];
+    this.getAllClasses();
+  }
+
+  ngAfterViewInit() {
+
   }
 
 }
